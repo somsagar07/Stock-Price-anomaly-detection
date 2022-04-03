50f9224583b69e45f61c42368bf3c5a610ad9647
Som Sagar <58594538+somsagar07@users.noreply.github.com>
2022-04-03 08:11:37 +0530

    LSTM_dynamic_thresholding

---
+++ main.py
@@ -0,0 +1,48 @@
+from orion import Orion
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
+
+#Importig Apple Stock data from yfinance
+appl_df = yf.download("AAPL", start="2019-01-01", end="2020-05-01")
+print(appl_df)
+#Getting Date and Adjusted Closing Price of Apple Stock
+appl_df_1 = appl_df.reset_index()[['Date','Adj Close']]
+
+#Making table with Date converted to timestamp in nanoseconds
+tmdata = pd.Series(appl_df_1['Date'].values.astype(np.int64))
+
+#Merging table with timestamp with original Table
+appl_df_2 = appl_df_1.merge(tmdata.rename('Timestamp'), how = 'inner', left_index= True, right_index = True)
+
+#Dropping Date
+appl_df_2.drop('Date', axis=1, inplace= True)
+
+#Converting table for model input
+appl_df_3 = appl_df_2.reindex(columns = ['Timestamp', 'Adj Close'])
+appl_final = appl_df_3.rename(columns={'Timestamp': 'timestamp', 'Adj Close': 'value'})
+appl_final['value'] = appl_final['value'] /appl_final['value'].abs().max()
+appl_final['timestamp'] = appl_final['timestamp'].div(1000000000)
+
+#INPUT
+print(appl_final)
+
+#LSTM
+hyperparameters = {
+    'keras.Sequential.LSTMTimeSeriesRegressor#1': {
+        'epochs': 5,
+        'verbose': True
+    }
+}
+
+orion = Orion(
+    pipeline='lstm_dynamic_threshold',
+    hyperparameters=hyperparameters
+)
+
+orion.fit(appl_final)
+
+#Output
+anomalies = orion.detect(appl_final)
+print(anomalies)
